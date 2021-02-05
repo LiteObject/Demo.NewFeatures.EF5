6ae9b5fb75c228846b89de3e7378894b4d6cbf1c
Mohammed <tuniis@live.com>
2021-02-05 14:33:39 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo.NewFeatures.EF5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
